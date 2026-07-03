2a1c97eb2f57c84f4e384a5a8b6ff9d47051a31a
TarasBilyi <taras.bilyi@gmail.com>
2026-07-03 22:26:16 +0300

    made base

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,11 +1,23 @@
 const openBtnEl = document.querySelector('[data-action="open"]');
 const closeBtnEl = document.querySelector('[data-action="close"]');
 const burgerMenuEl = document.querySelector('[data-visible]');
+const navLinks = document.querySelectorAll('[data-menu-link]');
+const body = document.querySelector('body');
 
 openBtnEl.addEventListener('click', e => {
   burgerMenuEl.dataset.visible = 'open';
+  body.classList.toggle('no-scroll');
 });
 
-closeBtnEl.addEventListener('click', e => {
-  burgerMenuEl.dataset.visible = 'close';
+closeBtnEl.addEventListener('click', closeMenu);
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    closeMenu();
+  });
 });
+
+function closeMenu() {
+  burgerMenuEl.dataset.visible = 'close';
+  body.classList.remove('no-scroll');
+}
